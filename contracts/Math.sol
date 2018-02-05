69c52416f7b41f10c1a1df23edef4f2dd9a98daf
Benny Sadeh <benny.sadeh@gmail.com>
2018-02-04 21:49:24 -0800

    Math

---
+++ contracts/Math.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.18;
+
+
+/* Math provides arithmetic functions for uint type pairs.
+  You can safely `add`, `subtract`, and `multiply` uint numbers without fear of integer overflow.
+  There is no `divide`, as divide-by-zero checking is built into the Solidity compiler.
+  You can also find the `min` and `max` of two numbers.
+*/
+library Math {
+
+  function plus(uint x, uint y) internal pure returns (uint z) { require((z = x + y) >= x); }
+  function minus(uint x, uint y) internal pure returns (uint z) { require((z = x - y) <= x); }
+
+  function multiply(uint x, uint y) internal pure returns (uint z) { require(y == 0 || (z = x * y) / y == x); }
+
+  function min(uint x, uint y) internal pure returns (uint) { return x <= y ? x : y; }
+  function max(uint x, uint y) internal pure returns (uint) { return x >= y ? x : y; }
+}
